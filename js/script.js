09588cf078c68d271ac7543dd7ba6513def2bb24
Danila Reznikov <reznikov.danila1999@gmail.com>
2022-03-17 11:41:39 +0300

    bootstrap style

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function printText( el ){
+
+    let letterTimeout = 30
+
+    let text = el.innerHTML
+    let i = 1
+
+    let print__fn = function(){
+
+        if( i <= text.length ){
+            el.innerHTML = text.substr( 0, i );
+            setTimeout( arguments.callee, letterTimeout );
+        }
+
+        i++;
+    }
+
+    print__fn() // init
+};
+
+// init
+let el = document.getElementById( 'animate-text' )
+printText( el );
